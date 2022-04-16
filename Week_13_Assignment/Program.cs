00087e824e2330761ba328e9f0eb95256a655475
djcyclonecoder <djcyclonecoder@gmail.com>
2022-04-16 13:00:13 -0500

    Add project files.

---
+++ Week_13_Assignment/Program.cs
@@ -0,0 +1,33 @@
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<Week_13_Assignment.Data.Week_13_AssignmentContext>(
+    options =>
+    {
+        options.UseSqlServer(builder.Configuration.GetConnectionString("Week_13_Assignment"));
+    });
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
